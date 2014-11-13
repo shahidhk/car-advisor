85e7dcff73a1d9b535c350b838679808d231fc26
Shahidh <muhammedshahid.k@gmail.com>
2014-11-13 15:10:07 +0530

    Added car data

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,73 +1,160 @@
 'use strict';
 
 angular.module('carAdvisor')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'key': 'angular',
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'key': 'browsersync',
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'key': 'gulp',
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'key': 'jasmine',
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'key': 'karma',
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'key': 'protractor',
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'key': 'jquery',
-        'title': 'jQuery',
-        'url': 'http://jquery.com/',
-        'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
-        'logo': 'jquery.jpg'
-      },
-      {
-        'key': 'bootstrap',
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'key': 'node-sass',
-        'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
-        'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
+    .controller('MainCtrl', function ($scope) {
+        $scope.carData = [
+            {
+                'name': 'Swift Dzire',
+                'description': '',
+                'logo':'dzire.png',
+                'petrol': {
+                  'cost': 485000,
+                  'hmileage': 19.1,
+                  'cmileage': 16.3,
+                  'maintainance': 3333.3
+                },
+                'diesel': {
+                  'cost': 579000,
+                  'hmileage': 23.4,
+                  'cmileage': 18.6,
+                  'maintainance': 4168.6
+                }
+              },
+              {
+                'name': 'Hyundai Xcent',
+                'description': '',
+                'logo':'xcent.png',
+                'petrol': {
+                  'cost': 473000,
+                  'hmileage': 19.2,
+                  'cmileage': 15.7,
+                  'maintainance': 2763
+                },
+                'diesel': {
+                  'cost': 562000,
+                  'hmileage': 24.4,
+                  'cmileage': 18.9,
+                  'maintainance': 4121.8
+                }
+              },
+              {
+                'name': 'Tata Manza',
+                'description': '',
+                'logo':'manza.png',
+                'petrol': {
+                  'cost': 580000,
+                  'hmileage': 14.5,
+                  'cmileage': 11.1,
+                  'maintainance': 3260
+                },
+                'diesel': {
+                  'cost': 600000,
+                  'hmileage': 21.2,
+                  'cmileage': 19.5,
+                  'maintainance': 5008
+                }
+              },
+              {
+                'name': 'Toyota Etios',
+                'description': '',
+                'logo':'etios.png',
+                'petrol': {
+                  'cost': 574000,
+                  'hmileage': 16.7,
+                  'cmileage': 13.5,
+                  'maintainance': 2963.3
+                },
+                'diesel': {
+                  'cost': 684000,
+                  'hmileage': 23.59,
+                  'cmileage': 20.3,
+                  'maintainance': 3070.8
+                }
+              },
+              {
+                'name': 'Honda Amaze',
+                'description': '',
+                'logo':'amaze.png',
+                'petrol': {
+                  'cost': 500000,
+                  'hmileage': 18,
+                  'cmileage': 15,
+                  'maintainance': 4557.5
+                },
+                'diesel': {
+                  'cost': 598000,
+                  'hmileage': 25.8,
+                  'cmileage': 21,
+                  'maintainance': 5140.9
+                }
+              },
+              {
+                'name': 'Fiat Linea',
+                'description': '',
+                'logo':'linea.png',
+                'petrol': {
+                  'cost': 700000,
+                  'hmileage': 14.9,
+                  'cmileage': 11.2,
+                  'maintainance': 4611
+                },
+                'diesel': {
+                  'cost': 817000,
+                  'hmileage': 20.4,
+                  'cmileage': 17.2,
+                  'maintainance': 5077
+                }
+              },
+              {
+                'name': 'Nissan Sunny',
+                'description': '',
+                'logo':'sunny.png',
+                'petrol': {
+                  'cost': 699000,
+                  'hmileage': 14.9,
+                  'cmileage': 11.2,
+                  'maintainance': 3249.1
+                },
+                'diesel': {
+                  'cost': 813000,
+                  'hmileage': 20.4,
+                  'cmileage': 17.2,
+                  'maintainance': 5881.3
+                }
+              },
+              {
+                'name': 'Honda City',
+                'description': '',
+                'logo':'city.png',
+                'petrol': {
+                  'cost': 699000,
+                  'hmileage': 14.9,
+                  'cmileage': 11.2,
+                  'maintainance': 3249.1
+                },
+                'diesel': {
+                  'cost': 813000,
+                  'hmileage': 20.4,
+                  'cmileage': 17.2,
+                  'maintainance': 5881.3
+                }
+              },
+                            {
+                'name': 'Custom',
+                'description': '',
+                'logo':'custom.png',
+                'petrol': {
+                  'cost': 0,
+                  'hmileage': 0,
+                  'cmileage': 0,
+                  'maintainance': 0
+                },
+                'diesel': {
+                  'cost': 0,
+                  'hmileage': 0,
+                  'cmileage': 0,
+                  'maintainance': 0
+                }
+            }
+        ];   
     });
-  });
